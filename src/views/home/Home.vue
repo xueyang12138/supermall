5d82c607cd6e54587caddf20020e0b3d6afefd8b
hkx <2362129787@qq.com>
2022-08-28 11:35:33 +0800

    添加首页某些组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,117 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
+    <RecommendView :recommends="recommends"></RecommendView>
+    <FeatureView></FeatureView>
+    <TabControl class="tab-control" :titles="['流行','新款','精选']"></TabControl>
+    <ul>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+
+
+    </ul>
+  </div>
 </template>
 
 <script>
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+import FeatureView from "@/views/home/childComps/FeatureView";
+
+import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/content/tabControl/TabControl";
+
+import {getHomeMultidata} from "@/network/home";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  #home {
+    padding-top: 44px;
+  }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommendItem">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: Array
+  }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0 20px;
+    border-bottom: 8px solid #eee;
+  }
+  .recommendItem {
+    flex: 1;
+  }
+  .recommendItem img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
